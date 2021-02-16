0f96e492677dacd603b5e3abd8c11ee00316ba4f
qhollis <58087502+qhollis@users.noreply.github.com>
2021-02-16 00:51:16 -0500

    Update lab_4.spec.js

--- cypress/integration/lab_tests/lab_4.spec.js
+++ cypress/integration/lab_tests/lab_4.spec.js
@@ -3,7 +3,7 @@ describe('Lab 4', () => {
   it('Successfully loads with valid HTML', () => {
     cy.fixture('test_values').then((json) => {
       const labUrl = `${json.test_context || ''}labs/lab_4/`;
-      cy.visit(labUrl); // change URL to match your dev URL
+      cy.visit('https://pacific-eyrie-03797.herokuapp.com/labs/lab_4/'); // change URL to match your dev URL
       cy.htmlvalidate();
     });
   });

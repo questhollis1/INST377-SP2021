f935a15f00cc88e8fe026bf3ba8b14f53ddbd177
qhollis <58087502+qhollis@users.noreply.github.com>
2021-02-18 18:20:32 -0500

    Update server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.route('/api')
     console.log('Form data in res.body', req.body);
     console.log('Now send something back to your client');
     // res.json({message: 'Hello World'});
-    res.send('Hello World');
+    res.send('hello world');
     console.log('hello123');
   });
 
